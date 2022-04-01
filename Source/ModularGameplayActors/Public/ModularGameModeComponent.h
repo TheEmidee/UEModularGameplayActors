d9b5ed4d20f8d77d7b2158927c030da38ec454e0
michael.delva <michael@emidee.net>
2022-04-01 11:46:01 +0200

    Created ModularGameModeComponent

---
+++ Source/ModularGameplayActors/Public/ModularGameModeComponent.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Components/GameFrameworkComponent.h>
+#include <CoreMinimal.h>
+
+#include "ModularGameModeComponent.generated.h"
+
+UCLASS()
+class MODULARGAMEPLAYACTORS_API UModularGameModeComponent : public UGameFrameworkComponent
+{
+    GENERATED_BODY()
+
+public:
+    template < class T >
+    T * GetGameMode() const
+    {
+        static_assert( TPointerIsConvertibleFromTo< T, AGameModeBase >::Value, "'T' template parameter to GetGameMode must be derived from AGameModeBase" );
+        return Cast< T >( GetOwner() );
+    }
+
+    template < class T >
+    T * GetGameModeChecked() const
+    {
+        static_assert( TPointerIsConvertibleFromTo< T, AGameModeBase >::Value, "'T' template parameter to GetGameModeChecked must be derived from AGameModeBase" );
+        return CastChecked< T >( GetOwner() );
+    }
+
+    virtual bool IsReadyToStartMatch() const;
+    virtual void HandleMatchHasStarted();
+};
